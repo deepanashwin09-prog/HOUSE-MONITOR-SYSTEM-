c6d2f041bda9989874a5cde872f0b8652199d38e
deepanashwin09-prog <deepanashwin09@gmail.com>
2026-08-25 21:36:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("HOUSE MONITOR SYSTEM started");
+let lightOn = false;
+
+function toggleLight() {
+
+    lightOn = !lightOn;
+
+    const lightStatus = document.getElementById("lightStatus");
+    const lightButton = document.getElementById("lightButton");
+
+    if (lightOn) {
+        lightStatus.textContent = "ON";
+        lightStatus.style.color = "green";
+        lightButton.textContent = "Turn OFF";
+    } else {
+        lightStatus.textContent = "OFF";
+        lightStatus.style.color = "";
+        lightButton.textContent = "Turn ON";
+    }
+}
