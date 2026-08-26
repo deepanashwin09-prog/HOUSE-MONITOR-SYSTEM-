b00bbba7774cce13c9df05d4ffae405c9ee21322
deepanashwin09-prog <deepanashwin09@gmail.com>
2026-08-25 21:42:47 -0700

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,31 @@ function toggleLight() {
         lightButton.textContent = "Turn ON";
     }
 }
+// ESP32 STATUS
+
+let esp32Online = true;
+
+function updateESP32Status() {
+
+    const indicator = document.getElementById("espIndicator");
+    const status = document.getElementById("espStatus");
+    const lastUpdate = document.getElementById("lastUpdate");
+
+    if (esp32Online) {
+
+        indicator.className = "indicator online";
+        status.textContent = "ONLINE";
+
+        lastUpdate.textContent =
+            "Last update: " + new Date().toLocaleTimeString();
+
+    } else {
+
+        indicator.className = "indicator offline";
+        status.textContent = "OFFLINE";
+
+        lastUpdate.textContent = "ESP32 not connected";
+    }
+}
+
+updateESP32Status();
